9ed1c17d4debffc2a37a0ad91423c5fa282fb71c
oabnerwilliam <abnerwillia026@gmail.com>
2025-02-27 14:31:10 -0300

    Colocando minha calculadora no Git

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const btns = document.querySelectorAll('.btn')
+const tela = document.querySelector('.tela')
+btns.forEach((btn) =>
+  btn.addEventListener('click', (e)=>{
+    const valor = e.target.value
+    if (valor!== '=') {
+        tela.innerText += valor
+        if (valor === 'C') {
+            tela.innerText = ''
+        }
+    } else {
+        // console.log(eval(tela.innerText))
+        try{
+            const resultado = eval(tela.innerText)  
+            if (!isFinite(resultado) || isNaN(resultado)) {
+                tela.innerText = "Erro.";
+            } else {
+                tela.innerText = resultado;
+            }  
+        } catch {
+            tela.innerText = "Erro."
+        }
+        
+    }
+  })
+)
